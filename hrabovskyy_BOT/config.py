13451afc162eac213f9433d14e212d68db10fd06
Andrii Hrabovskyy <flunks.divans-0s@icloud.com>
2025-06-04 19:48:22 +0300

    Update config.py

--- hrabovskyy_BOT/config.py
+++ hrabovskyy_BOT/config.py
@@ -1,5 +1,5 @@
-BOT_TOKEN = "7721416039:AAEDgaICrPJAk2jxOVtbqN9NgTUVI0oCyCU"
-API_BASE_URL = "http://localhost:8080/api"
+BOT_TOKEN = "key"
+API_BASE_URL = "key"
 
 DAILY_NEWS_HOUR = 9  # година для щоденної розсилки
 LOG_FILE = "logs/errors.log"
